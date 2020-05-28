6ef26a2dc33d655866beaeda1841f33afd75d97c
Omar Pino <matato_357@hotmail.com>
2020-05-28 13:47:25 -0700

    Procedural assets

---
+++ Assets/ProceduralGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ProceduralGenerator : MonoBehaviour
+{
+    [SerializeField] int size = 256;
+    [SerializeField] float offsetX = 0;
+    [SerializeField] float offsetY = 0;
+    [SerializeField] GameObject dirtBlock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Generate()
+    {
+        float width = dirtBlock.transform.lossyScale.x;
+        float height = dirtBlock.transform.lossyScale.y;
+
+        float perlin = Mathf.PerlinNoise(width, height);
+    }
+}
