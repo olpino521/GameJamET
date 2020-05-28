bea9e822b273cc61c66d7af71e10b17996586cbd
Deryana <deredhuzent@gmail.com>
2020-05-28 15:06:25 -0700

    Voice non finished

---
+++ Assets/Scripts/Voice/PlayerActions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    [SerializeField]
+    private float move = 1f;
+
+    public void Jump()
+    {
+        transform.position += Vector3.up * move;
+    }
+    
+    public void MoveLeft()
+    {
+        transform.position += Vector3.left * move;
+    }
+}
