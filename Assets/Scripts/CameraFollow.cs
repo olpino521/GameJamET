64d872ae7bca4a8b8a2492822345e05fa14a7fe9
Omar Pino <matato_357@hotmail.com>
2020-05-28 15:58:26 -0700

    GameDone

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float distance;
+    [SerializeField] private float viewYOffset;
+    Vector3 referenceDistance;
+
+    private void LateUpdate()
+    {
+        FollowTarget();
+    }
+
+    // Start is called before the first frame update
+    void FollowTarget()
+    {
+        float cameraHeight = target.position.y + Mathf.Sqrt(2 * distance * distance);
+        float referenceZ = target.position.z - distance;
+        float referenceX = target.position.x - distance;
+        referenceDistance = new Vector3(referenceX, cameraHeight + viewYOffset, referenceZ);
+        transform.position = referenceDistance;
+    }
+}
--- Assets/Scripts/Voice/PlayerActions.cs
+++ Assets/Scripts/Voice/PlayerActions.cs
@@ -1,28 +1,41 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerActions : MonoBehaviour
 {
+    [SerializeField] Menu menuRef;
     [SerializeField]
     private float forward = 8f;
     [SerializeField]
     private float jump = 8f;
 
-    private Rigidbody rb;
+    private Rigidbody2D rb;
 
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void Jump()
     {
-        rb.AddForce(new Vector3(0f, jump + 5f, 0f), ForceMode.Impulse);
+        rb.AddForce(new Vector2(0f, jump),ForceMode2D.Impulse);
     }
 
     public void MoveForward()
     {
-        rb.AddForce(new Vector3(forward + 5f, 0f, 0f), ForceMode.Impulse);
+        rb.AddForce(new Vector2(forward, 0f), ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Death"))
+        {
+            menuRef.GameOver("You Lose");
+        }
+        else if (collision.CompareTag("Win"))
+        {
+            menuRef.GameOver("You Win");
+        }
     }
 }
