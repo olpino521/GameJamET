23b653e139dc64a9db125672cf5d39b2c622b97f
Omar Pino <matato_357@hotmail.com>
2020-05-28 15:36:19 -0700

    Win and Lose conditions

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] GameObject playerRef;
+    [SerializeField] Menu menuHandler;
+    [SerializeField] string objectName;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (objectName.Equals("Death"))
+        {
+            menuHandler.GameOver("You Lose");
+        }
+        else if (objectName.Equals("Win"))
+        {
+            menuHandler.GameOver("You Win");
+        }
+    }
+}
